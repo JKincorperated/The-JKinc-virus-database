7bbe652479c1306f5c6572f45888e1cbbca41f4f
JKincorperated <73645805+JKincorperated@users.noreply.github.com>
2021-02-01 15:15:56 +0000

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -1,4 +1,5 @@
 @echo off
+cls
 Echo Checking For Updates
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://raw.githubusercontent.com/JKincorperated/The-JKinc-virus-database/main/version.txt', 'version.txt')"
 set /p mytextfile=< version.txt
@@ -7,6 +8,7 @@ if NOT %mytextfile%==1.0.3 Updater.bat
 timeout /t 1 /NOBREAK > nul
 del version.txt
 cls
+Echo.
 Echo JKinc AntiVirus
 echo.
 echo Please press enter to continue
